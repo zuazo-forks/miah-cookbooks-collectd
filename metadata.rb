757774eb47ddbffcc733c7cf0b32f6aa83c26d3b
Miah Johnson <miah@cx.com>
2012-05-05 08:50:30 -0700

    Update of metadata details.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
-maintainer       "Noan Kantrowitz"
-maintainer_email "noah@coderanger.net"
+maintainer       "Miah Johnson"
+maintainer_email "miah@chia-pet.org"
 license          "Apache 2.0"
 description      "Install and configure the collectd monitoring daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 supports         "ubuntu"
 
-depends "collectd"
-recommends "apache2"
+%w[ build-essential ark runit ].each do |cookbook|
+  depends cookbook
+end
