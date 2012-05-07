8478235d464b29f9301c50a105761a5107888882
Miah Johnson <miah@cx.com>
2012-05-06 19:49:11 -0700

    Add name to metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "Miah Johnson"
 maintainer_email "miah@chia-pet.org"
 license          "Apache 2.0"
+name             "collectd"
 description      "Install and configure the collectd monitoring daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
