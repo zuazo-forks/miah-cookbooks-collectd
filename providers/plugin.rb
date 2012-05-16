c459c4215f14a411f946514a33847f5304d43a17
Miah Johnson <miah@cx.com>
2012-05-16 13:24:15 -0700

    Remove definition for collectd_plugin and re-implement it as lwrp.

---
+++ providers/plugin.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: collectd
+# Provider:: plugin
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :create do
+  template new_resource.name do
+    path "#{node['collectd']['plugconf_dir']}/#{new_resource.name}.conf"
+    owner "root"
+    group "root"
+    mode 0644
+    if params[:template].nil?
+      source "plugin.conf.erb"
+      cookbook params[:cookbook] || "collectd"
+    else
+      source params[:template]
+      cookbook params[:cookbook]
+    end
+    variables :name=>new_resource.name, :options=>params[:options]
+    notifies :restart, "service[collectd]", :delayed
+  end
+  new_resource.updated_by_last_action(true)
+end
+
+action :delete do
+  file new_resource.name do
+    path "#{node['collectd']['plugconf_dir']}/#{new_resource.name}.conf"
+    action :delete 
+  end
+  new_resource.updated_by_last_action(true)
+end
