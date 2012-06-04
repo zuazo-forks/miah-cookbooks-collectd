d990756a13aa4d35977474f3f1c4e9ffc441ab2d
Miah Johnson <miah@cx.com>
2012-06-03 18:52:18 -0700

    use single line for supports/depends blocks.

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,6 @@ name             "collectd"
 description      "Install and configure the collectd monitoring daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
-supports         "ubuntu"
 
-%w( build-essential apt ark runit yum ).each do |cookbook|
-  depends cookbook
-end
+%w( ubuntu centos ).each { |os| supports os }
+%w( build-essential apt ark runit yum ).each { |cookbook| depends cookbook }
