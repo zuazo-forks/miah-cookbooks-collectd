5855db3c4be5274ce564fced4dfc2a2eeec6619b
Miah Johnson <miah@cx.com>
2012-10-04 17:10:44 -0700

    Really rough collectd_test cookbook.

---
+++ test/kitchen/cookbooks/collectd_test/metadata.rb
@@ -0,0 +1,9 @@
+name 'collectd_test'
+license 'Apache 2.0'
+version '0.1.0'
+description 'Acceptance tests for collectd.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Miah Johnson'
+maintainer_email 'miah@chia-pet.org'
+
+%w(apt ark build-essential logrotate runit yum).each { |cookbook| depends cookbook }
