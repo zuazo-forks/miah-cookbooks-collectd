e8f01504acf945ce882bfc1d5cd71cccec8d78f4
Miah Johnson <miah@hoteltonight.com>
2012-10-31 15:17:46 -0700

    correct search parameters for chef_environment and allow for configuration during chef solo.

--- recipes/client_graphite.rb
+++ recipes/client_graphite.rb
@@ -5,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,17 +21,16 @@ include_recipe "collectd::_server_runit"
 include_recipe "collectd::_server_conf"
 
 servers = []
-
 if Chef::Config[:solo]
-  Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
+  if node['graphite']['server_address']
+    servers << node['graphite']['server_address']
+  else
+    servers << '127.0.0.1'
+  end
 else
-  search(:node, "roles:#{node['graphite']['server_role']} AND chef_environment:#{node.chef_environment}") do |n|
+  search(:node, "role:#{node['graphite']['server_role']} AND chef_environment:#{node.chef_environment}") do |n|
     servers << n['fqdn']
   end
-end
-
-if servers.empty?
-  raise "No graphite servers found! Please configure at least one node with role: \"#{node['graphite']['server_role']}\" in environment: \"#{node.chef_environment}\"."
 end
 
 collectd_plugin "write_graphite" do
