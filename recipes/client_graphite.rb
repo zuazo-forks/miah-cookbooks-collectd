e11fd3cf22cf0043214d3e2edf7f8ca31c83eee1
miah <miah@hoteltonight.com>
2012-11-28 22:49:03 -0800

    we should reference ipaddress rather than fqdn.

--- recipes/client_graphite.rb
+++ recipes/client_graphite.rb
@@ -25,7 +25,7 @@ if Chef::Config[:solo]
   end
 else
   search(:node, "role:#{node['graphite']['server_role']} AND chef_environment:#{node.chef_environment}") do |n|
-    server = n['fqdn']
+    server = n['ipaddress']
   end
 end
 
