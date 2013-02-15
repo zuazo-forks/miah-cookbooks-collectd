8df6481784da6fd82da15ad15352fdc91788cafc
Chris Lundquist <chris.lundquist@bluebox.net>
2013-02-15 14:38:22 -0800

    [data_bag_plugins] add data_bag based plugin recipe

---
+++ recipes/data_bag_plugins.rb
@@ -0,0 +1,11 @@
+bag = node.collectd.data_bag_name
+
+node.collectd.plugins.each do |instance|
+  instance_data = data_bag_item( bag, instance )
+
+  collectd_plugin instance do
+    instance_data.each do |attribute,value|
+      send(attribute, value) unless attribute == "id"
+    end
+  end
+end
