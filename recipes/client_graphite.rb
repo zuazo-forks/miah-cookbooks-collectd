8dbf9f1bebfefda348551cd22a06edceb7e96db0
Greg Fitzgerald <greg@gregf.org>
2014-05-05 23:23:27 -0400

    Fix bug where graphite url could not be set

--- recipes/client_graphite.rb
+++ recipes/client_graphite.rb
@@ -28,9 +28,12 @@ elsif node['graphite']['server_role']
   Chef::Log.warn("node with role #{node['graphite']['server_role']} not found") unless server
 end
 
-server = node['graphite']['server_address']
-Chef::Log.warn("node.graphite.server_address not set. defaulting to 127.0.0.1") unless server
-server = "127.0.0.1"
+if node['graphite']
+  server = node['graphite']['server_address']
+else
+  Chef::Log.warn("node.graphite.server_address not set. defaulting to 127.0.0.1")
+  server = "127.0.0.1"
+end
 
 if node['collectd']['version'] =~ /5\.\d+/
   collectd_plugin 'write_graphite' do
