35e3dad24ce62a2620eec443ebaeca57c67cd92b
Xabier de Zuazo <xabier@onddo.com>
2014-11-11 19:47:06 +0100

    Fix ::_install_from_package ChefSpec tests

--- spec/unit/recipes/_install_from_package_spec.rb
+++ spec/unit/recipes/_install_from_package_spec.rb
@@ -1,18 +1,24 @@
 require 'spec_helper'
 
 describe 'collectd::_install_from_package' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_runner) { ChefSpec::Runner.new }
+  let(:chef_run) { chef_runner.converge(described_recipe) }
+  let(:node) { chef_runner.node }
 
-  it 'should include apt on debian' do
-    chef_run.node.set['platform_family'] = 'debian'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to include_recipe('apt')
+  context 'on Debian' do
+    before { node.automatic['platform_family'] = 'debian' }
+
+    it 'should include apt on debian' do
+      expect(chef_run).to include_recipe('apt')
+    end
   end
 
-  it 'should include yum-epel on rhel' do
-    chef_run.node.set['platform_family'] = 'rhel'
-    chef_run.converge(described_recipe)
-    expect(chef_run).to include_recipe('yum-epel')
+  context 'on RedHat' do
+    before { node.automatic['platform_family'] = 'rhel' }
+
+    it 'should include yum-epel' do
+      expect(chef_run).to include_recipe('yum-epel')
+    end
   end
 
   it 'runs a execute kill collectdmon' do
